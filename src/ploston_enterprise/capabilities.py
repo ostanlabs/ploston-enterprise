d249766ddc53df7093e6fb31858502d678d6c482
Marc Ostan <marc.ostan@avast.com>
2026-01-21 23:59:21 -0500

    Fix EnterpriseCapabilitiesProvider to use correct FeatureFlags fields
    - Remove LicenseInfo import from ploston_core (doesn't exist)
    - Make license_info optional in constructor
    - Use correct field names: max_concurrent_executions, max_workflows, telemetry_retention_days
    - Add parallel_execution to features dict

--- src/ploston_enterprise/capabilities.py
+++ src/ploston_enterprise/capabilities.py
@@ -1,8 +1,9 @@
 """Enterprise capabilities provider for Ploston Enterprise."""
 
+from typing import Optional
+
 from ploston_core.extensions.capabilities import (
     Capabilities,
-    LicenseInfo as CapLicenseInfo,
     CapabilitiesProvider,
 )
 from ploston_core.extensions import FeatureFlagRegistry, PluginRegistry
@@ -12,19 +13,29 @@ from .license import LicenseInfo
 
 class EnterpriseCapabilitiesProvider(CapabilitiesProvider):
     """Enterprise capabilities provider.
-    
+
     Returns full feature set and license info.
     """
-    
-    def __init__(self, license_info: LicenseInfo, version: str):
+
+    def __init__(self, version: str, license_info: Optional[LicenseInfo] = None):
         self._license = license_info
         self._version = version
-    
+
     def get_capabilities(self) -> Capabilities:
         """Get enterprise capabilities based on license."""
         flags = FeatureFlagRegistry.flags()
         plugins = PluginRegistry.get().get_enabled_features()
-        
+
+        # Build license dict if license info is available
+        license_dict = None
+        if self._license:
+            license_dict = {
+                "id": self._license.id,
+                "expires": self._license.expires.isoformat() if self._license.expires else None,
+                "seats": self._license.seats,
+                "features": self._license.features,
+            }
+
         return Capabilities(
             tier="enterprise",
             version=self._version,
@@ -36,16 +47,13 @@ class EnterpriseCapabilitiesProvider(CapabilitiesProvider):
                 "policy": flags.policy,
                 "patterns": flags.patterns,
                 "synthesis": flags.synthesis,
+                "parallel_execution": flags.parallel_execution,
             },
             limits={
-                "max_concurrent_workflows": flags.max_concurrent_workflows,
-                "max_workflow_steps": flags.max_workflow_steps,
+                "max_concurrent_executions": flags.max_concurrent_executions,
+                "max_workflows": flags.max_workflows,
+                "telemetry_retention_days": flags.telemetry_retention_days,
             },
-            license=CapLicenseInfo(
-                id=self._license.id,
-                expires=self._license.expires.isoformat(),
-                seats=self._license.seats,
-                features=self._license.features,
-            ),
+            license=license_dict,
         )
 
